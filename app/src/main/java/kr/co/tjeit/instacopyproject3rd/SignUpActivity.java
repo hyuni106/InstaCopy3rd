b2bf666dfc2c7df28510c49195a00bda5e6a6a87
kdy101289 <kdy101289@gmail.com>
2017-09-05 17:51:11 +0900

    회원가입화면 구현 좋아요(팔로우, 내 소식) 화면 구현 중.

--- app/src/main/java/kr/co/tjeit/instacopyproject3rd/SignUpActivity.java
+++ app/src/main/java/kr/co/tjeit/instacopyproject3rd/SignUpActivity.java
@@ -2,15 +2,12 @@ package kr.co.tjeit.instacopyproject3rd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_sign_up);
     }
 }
---
+++ app/src/main/java/kr/co/tjeit/instacopyproject3rd/adapter/FollowingAdapter.java
@@ -0,0 +1,47 @@
+package kr.co.tjeit.instacopyproject3rd.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import kr.co.tjeit.instacopyproject3rd.R;
+import kr.co.tjeit.instacopyproject3rd.data.Like;
+
+/**
+ * Created by the on 2017-09-05.
+ */
+
+public class FollowingAdapter extends ArrayAdapter<Like> {
+
+    Context mContext;
+    List<Like> mList;
+    LayoutInflater inf;
+
+    public FollowingAdapter(Context context, List<Like> list) {
+        super(context, R.layout.following_fragment_list_item, list);
+        mContext = context;
+        mList = list;
+        inf = LayoutInflater.from(mContext);
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+        if (row == null) {
+            // 거의 반드시 딱 한줄만 if문 안에 들어옴.
+            row = inf.inflate(R.layout.following_fragment_list_item, null);
+        }
+
+        Like data = mList.get(position);
+
+        return row;
+    }
+}
