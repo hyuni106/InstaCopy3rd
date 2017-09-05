bef57242aceed448895f280c3b788f1c9ca8bcb5
unripeblue <syp06@naver.com>
2017-09-05 16:33:42 +0900

    뉴스피드 리스트 그리는것까지만

--- app/src/main/java/kr/co/tjeit/instacopyproject3rd/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/instacopyproject3rd/MainActivity.java
@@ -4,12 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 //테스팅
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
     }
 }
