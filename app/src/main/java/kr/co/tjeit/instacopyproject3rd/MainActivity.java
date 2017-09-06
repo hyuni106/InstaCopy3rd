dbbdecbffcaee8907c59b9fa00ccdb8f42c25979
hyuni106 <30651511+hyuni106@users.noreply.github.com>
2017-09-06 09:57:39 +0900

    Merge remote-tracking branch 'remotes/origin/khc'
    # Conflicts:
    #	.idea/vcs.xml
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/kr/co/tjeit/instacopyproject3rd/BaseActivity.java
    #	app/src/main/java/kr/co/tjeit/instacopyproject3rd/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/kr/co/tjeit/instacopyproject3rd/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/instacopyproject3rd/MainActivity.java
@@ -1,28 +1,33 @@
 package kr.co.tjeit.instacopyproject3rd;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.os.Bundle;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    private android.widget.Button tmpBtn;
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.tmpBtn = (Button) findViewById(R.id.tmpBtn);
+        bindViews();
+        setupEvents();
+        setValues();
 
-        tmpBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-                startActivity(intent);
-            }
-        });
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
     }
 }
