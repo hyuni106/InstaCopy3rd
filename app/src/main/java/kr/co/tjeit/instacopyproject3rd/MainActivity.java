8223a0aa264d81f0371e7a96e69ea15d147a6491
unripeblue <syp06@naver.com>
2017-09-06 09:43:12 +0900

    Master와 Merge 작업 완료 직후.

--- app/src/main/java/kr/co/tjeit/instacopyproject3rd/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/instacopyproject3rd/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends BaseActivity {
 
     private android.widget.Button tmpBtn;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
