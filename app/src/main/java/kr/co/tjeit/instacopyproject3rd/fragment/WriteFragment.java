56f82587f9997ee43caf6e69d990adaa8d716f81
ziO <zio4272@gmail.com>
2017-09-07 00:56:56 +0900

    WriteFragment
    첫번째 아이템 생성완료 (갤러리)

--- app/src/main/java/kr/co/tjeit/instacopyproject3rd/fragment/WriteFragment.java
+++ app/src/main/java/kr/co/tjeit/instacopyproject3rd/fragment/WriteFragment.java
@@ -1,13 +1,19 @@
 package kr.co.tjeit.instacopyproject3rd.fragment;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TabWidget;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,16 +30,24 @@ public class WriteFragment extends Fragment {
 
     WriteAdapter mAdapter;
     List<Post> postList = new ArrayList<>();
-    private ListView newsfeedListView;
-    private android.widget.LinearLayout newsfeedLayout;
-    private ListView writeListView;
-    private LinearLayout writeLayout;
+    private LinearLayout tab1;
+    private LinearLayout tab2;
+    private LinearLayout tab3;
+    private FrameLayout tabcontent;
+    private TabWidget tabs;
+    private TabHost tabHost;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_write_item, container, false);
-        this.writeListView = (ListView) v.findViewById(R.id.writeListView);
+        this.tabHost = (TabHost) v.findViewById(R.id.tabHost);
+        this.tabs = (TabWidget) v.findViewById(android.R.id.tabs);
+        this.tabcontent = (FrameLayout) v.findViewById(android.R.id.tabcontent);
+        this.tab3 = (LinearLayout) v.findViewById(R.id.tab3);
+        this.tab2 = (LinearLayout) v.findViewById(R.id.tab2);
+        this.tab1 = (LinearLayout) v.findViewById(R.id.tab1);
 
 
         return v;
@@ -42,8 +56,29 @@ public class WriteFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        makeTabHost();
         setupEvents();
         setValuse();
+    }
+
+    private void makeTabHost() {
+        // 탭호스트를 사용하기 위해서는 반드시 setup을 먼저 진행해야함
+        tabHost.setup();
+
+        // 탭에 들어가는 버튼(tabspec)을 생성하는 작업
+        // 구별자(tab1), 표시(지역) 세팅
+        TabHost.TabSpec ts1 = tabHost.newTabSpec("tab1").setIndicator("갤러리");
+        ts1.setContent(R.id.tab1);
+        tabHost.addTab(ts1);
+
+        TabHost.TabSpec ts2 = tabHost.newTabSpec("tab2").setIndicator("사진");
+        ts2.setContent(R.id.tab2);
+        tabHost.addTab(ts2);
+
+        TabHost.TabSpec ts3 = tabHost.newTabSpec("tab3").setIndicator("동영상");
+        ts3.setContent(R.id.tab3);
+        tabHost.addTab(ts3);
+
     }
 
     private void setValuse() {
@@ -54,6 +89,9 @@ public class WriteFragment extends Fragment {
     }
 
     private void setupEvents() {
+
+
+
 
     }
 }
