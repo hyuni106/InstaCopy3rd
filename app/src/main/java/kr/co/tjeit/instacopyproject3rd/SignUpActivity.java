ea728c6cb082a6ea64ff1b9c9c8891ba38828038
kdy101289 <kdy101289@gmail.com>
2017-09-07 10:49:09 +0900

    SignActivity에서 next 버튼 클릭시 SignCheckActivity로 화면 넘어가게 구현 SignActivty 이름, 비밀번호 빈 칸시 화면 Intent되지 않고 입력 알리는 토스트 구현.

--- app/src/main/java/kr/co/tjeit/instacopyproject3rd/SignUpActivity.java
+++ app/src/main/java/kr/co/tjeit/instacopyproject3rd/SignUpActivity.java
@@ -3,17 +3,26 @@ package kr.co.tjeit.instacopyproject3rd;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import kr.co.tjeit.instacopyproject3rd.util.SignUpCheckActivity;
+
 //dd
 public class SignUpActivity extends BaseActivity {
 
     private android.widget.TextView nextTxt;
+    private de.hdodenhof.circleimageview.CircleImageView signUpImg;
+    private android.widget.EditText nameEdt;
+    private android.widget.EditText passwordEdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        this.nextTxt = (TextView) findViewById(R.id.nextTxt);
+
         bindViews();
         setupEvents();
         setValues();
@@ -24,9 +33,17 @@ public class SignUpActivity extends BaseActivity {
         nextTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                TODO - 합치고나서 실행
-//                Intent intent = new Intent(mContext, UserNameMakeActivity.class);
-//                startActivity(intent);
+
+                if (nameEdt.getText().toString().equals("")) {
+                    Toast.makeText(mContext, "이름을 입력해주세요", Toast.LENGTH_SHORT).show();
+                }
+                else if (passwordEdt.getText().toString().equals("")) {
+                    Toast.makeText(mContext, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                }
+                else if (!(nameEdt.getText().toString().equals("")) && !(passwordEdt.getText().toString().equals(""))) {
+                    Intent intent = new Intent(mContext, SignUpCheckActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
@@ -39,6 +56,9 @@ public class SignUpActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
-
+        this.passwordEdt = (EditText) findViewById(R.id.passwordEdt);
+        this.nameEdt = (EditText) findViewById(R.id.nameEdt);
+        this.signUpImg = (CircleImageView) findViewById(R.id.signUpImg);
+        this.nextTxt = (TextView) findViewById(R.id.nextTxt);
     }
 }
