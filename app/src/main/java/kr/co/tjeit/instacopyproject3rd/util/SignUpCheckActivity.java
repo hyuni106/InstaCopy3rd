f670ab4bc3ffe53a4ed199ce9d0eb15c208a4ec4
kdy101289 <kdy101289@gmail.com>
2017-09-07 10:17:25 +0900

    회원가입 버튼 클릭하면 넘어갈 Activity 생성 및 xml 구현.

---
+++ app/src/main/java/kr/co/tjeit/instacopyproject3rd/util/SignUpCheckActivity.java
@@ -0,0 +1,34 @@
+package kr.co.tjeit.instacopyproject3rd.util;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.co.tjeit.instacopyproject3rd.BaseActivity;
+import kr.co.tjeit.instacopyproject3rd.R;
+
+public class SignUpCheckActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_check);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
