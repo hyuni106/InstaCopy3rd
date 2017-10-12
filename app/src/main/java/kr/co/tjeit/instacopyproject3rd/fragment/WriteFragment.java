fb5da2a8b592105fc9df3c4088875e321249cddf
hyuni106 <30651511+hyuni106@users.noreply.github.com>
2017-10-12 12:10:45 +0900

    사진 촬영 후 포스팅 등록 완료

--- app/src/main/java/kr/co/tjeit/instacopyproject3rd/fragment/WriteFragment.java
+++ app/src/main/java/kr/co/tjeit/instacopyproject3rd/fragment/WriteFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -43,6 +44,7 @@ public class WriteFragment extends Fragment {
 
     WriteAdapter mAdapter;
     List<Post> postList = new ArrayList<>();
+    private android.support.design.widget.TabLayout mTab;
 
 
     @Nullable
@@ -53,59 +55,18 @@ public class WriteFragment extends Fragment {
         this.mainViewPager = (ViewPager) v.findViewById(R.id.mainViewPager);
         this.titleFramLayout = (FrameLayout) v.findViewById(R.id.titleFramLayout);
         this.menuTxt = (TextView) v.findViewById(R.id.menuTxt);
-        this.tabHost = (TabHost) v.findViewById(R.id.tabHost);
-        this.tabs = (TabWidget) v.findViewById(android.R.id.tabs);
-        this.tabcontent = (FrameLayout) v.findViewById(android.R.id.tabcontent);
-        this.tab3 = (LinearLayout) v.findViewById(R.id.tab3);
-        this.tab2 = (LinearLayout) v.findViewById(R.id.tab2);
-        this.tab1 = (LinearLayout) v.findViewById(R.id.tab1);
+        this.mTab = (TabLayout) v.findViewById(R.id.mTab);
         return v;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        makeTabHost();
         setupEvents();
         setValuse();
     }
 
-    private void makeTabHost() {
-        // 탭호스트를 사용하기 위해서는 반드시 setup을 먼저 진행해야함
-        tabHost.setup();
-
-        // 탭에 들어가는 버튼(tabspec)을 생성하는 작업
-        // 구별자(tab1), 표시(지역) 세팅
-        TabHost.TabSpec ts1 = tabHost.newTabSpec("tab1").setIndicator("갤러리");
-        ts1.setContent(R.id.tab1);
-        tabHost.addTab(ts1);
-
-        TabHost.TabSpec ts2 = tabHost.newTabSpec("tab2").setIndicator("사진");
-        ts2.setContent(R.id.tab2);
-        tabHost.addTab(ts2);
-
-        TabHost.TabSpec ts3 = tabHost.newTabSpec("tab3").setIndicator("동영상");
-        ts3.setContent(R.id.tab3);
-        tabHost.addTab(ts3);
-
-    }
-
-
     private void setupEvents() {
-//        탭의 태그를 받아와 ViewPager의 페이지 변경
-        View.OnClickListener pageChangeListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pageNum = Integer.parseInt(v.getTag().toString());
-                mainViewPager.setCurrentItem(pageNum);
-
-            }
-        };
-
-        tab1.setOnClickListener(pageChangeListener);
-        tab2.setOnClickListener(pageChangeListener);
-        tab3.setOnClickListener(pageChangeListener);
-
         mainViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -114,17 +75,13 @@ public class WriteFragment extends Fragment {
 
             @Override
             public void onPageSelected(int position) {
-
-                // 선택된 탭으로 이동
-                tabHost.setCurrentTab(position);
-
                 if (position == 0) {
                     menuTxt.setText("갤러리");
                 } else if (position == 1) {
                     menuTxt.setText("사진");
 //                    카메라 화면 실행
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivity(intent);
+//                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                    startActivityForResult(intent, 1);
                 } else if (position == 2) {
                     menuTxt.setText("동영상");
 //                    카메라 동영상 촬영 화면 실행
@@ -143,7 +100,7 @@ public class WriteFragment extends Fragment {
 
     private void setValuse() {
         mainViewPager.setAdapter(new MyPagerAdapter(getFragmentManager()));
-
+        mTab.setupWithViewPager(mainViewPager);
     }
 
     class MyPagerAdapter extends FragmentStatePagerAdapter {
@@ -160,12 +117,25 @@ public class WriteFragment extends Fragment {
         public Fragment getItem(int position) {
 
             if (position == 0) {
-
                 return new WritePostFragment();
             } else if (position == 1)
-                return new MyprofileFragment();
+                return new CameraFragment();
             else {
                 return new MyprofileFragment();
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "갤러리";
+                case 1:
+                    return "사진";
+                case 2:
+                    return "동영상";
+                default:
+                    return null;
             }
         }
     }
